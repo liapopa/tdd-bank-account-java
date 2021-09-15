09c418961020bb82450ce486ea4a6b9c174a7076
Alex P <popa.alex23@gmail.com>
2021-09-15 14:59:30 +0300

    added new Test

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -4,12 +4,25 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+//import static org.hamcrest.*;
 
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+	//@Disabled
+    public void depositAnAmountToIncreaseTheBalance() throws Exception {
+
+        //arrange
+        Account account = new Account();
+        account.deposit(10);
+        assertThat(account.balance);
+        //act (add some money)
+
+
+        //assert
+
+
+        //assertThat("your first test").isBlank();
+        //if ("gd".contains("d")) throw new Exception("Exception !!!");
     }
 }
